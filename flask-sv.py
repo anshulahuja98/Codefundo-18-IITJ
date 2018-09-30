c4998a8679cfed8dec6e013eef1d0507a61e018c
Anshul Ahuja <36476228+anshulahuja98@users.noreply.github.com>
2018-09-30 18:26:51 +0530

    Flask server basic boilerplate

---
+++ flask-sv.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/post', methods=["POST"])
+def post():
+    print(request.data)
+    return ''
+
+
+@app.route('/get', methods=["GET"])
+def get():
+    return 'GET'
+
+
+app.run(host='0.0.0.0', port=8090, debug=True)
