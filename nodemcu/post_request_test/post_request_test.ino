222f4a0ea7f89b4c12d9191b80c459ab888c8bb5
aakshjn <35147271+aakshjn@users.noreply.github.com>
2018-09-30 18:51:13 +0530

    Post request sample code nodemcu

---
+++ nodemcu/post_request_test/post_request_test.ino
@@ -0,0 +1,43 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+ 
+void setup() {
+ 
+  Serial.begin(115200);                                  //Serial connection
+  WiFi.begin("null", "patanahi1");   //WiFi connection
+ 
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+ 
+    delay(500);
+    Serial.println("Waiting for connection");
+ 
+  }
+ 
+}
+ 
+void loop() {
+ 
+ if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+ 
+   HTTPClient http;    //Declare object of class HTTPClient
+ 
+   http.begin("http://192.168.31.7:8090/post");      //Specify request destination
+   http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+ 
+   int httpCode = http.POST("Message from nodemcu");   //Send the request
+   String payload = http.getString();                  //Get the response payload
+ 
+   Serial.println(httpCode);   //Print HTTP return code
+   Serial.println(payload);    //Print request response payload
+ 
+   http.end();  //Close connection
+ 
+ }else{
+ 
+    Serial.println("Error in WiFi connection");   
+ 
+ }
+ 
+  delay(30000);  //Send a request every 30 seconds
+ 
+}
