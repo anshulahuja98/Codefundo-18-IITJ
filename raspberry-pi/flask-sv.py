f54a6cce017bf25cd7bd1698390eb1d26a691c5a
Aksh Chordia <35147271+aakshjn@users.noreply.github.com>
2018-10-23 18:27:34 +0530

    Added audio siren, and other checks

--- raspberry-pi/flask-sv.py
+++ raspberry-pi/flask-sv.py
@@ -1,17 +1,32 @@
+import RPi.GPIO as GPIO
+import time
 from flask import Flask, request
-#curl -i -H "Content-Type: application/json" -X POST -d '{"userId":"1", "username": "fizz bizz"}' http://localhost:8090/post
+import vlc
+#curl -i -H "Content-Type: application/json" -X POST -d '{"userId":"1", "userna$
 app = Flask(__name__)
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW)
 
 @app.route('/post', methods=["POST"])
-def post():
-    print(request.data)
+def postJsonHandler():
+    data = request.get_json()
+    print(data['values'])
+    mod_ax= abs(data['values'][0])
+    mod_ay= abs(data['values'][1])
+    mod_az= abs(data['values'][2])
+    if mod_ax>1.5 or mod_ay>1.5 or mod_az>1.5:
+        print("Earthquake Detected")
+        GPIO.output(18,GPIO.HIGH)
+        GPIO.output(19,GPIO.HIGH)
+        player = vlc.MediaPlayer("/home/pi/CFD-18/raspberry-pi/siren.mp3")
+        player.play()
+        time.sleep(24)
+    else:
+        print("You are safe")
     return ''
-
-
 @app.route('/get', methods=["GET"])
 def get():
     return 'GET'
-
 
 app.run(host='0.0.0.0', port=8090, debug=True)
