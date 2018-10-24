42c9300a63686be2296c37b07a0592d4cc17b888
Ubuntu <darkside@main-sv.b0ovjr353ggu1h2h0pdfs3jd2d.bx.internal.cloudapp.net>
2018-10-24 14:03:47 +0000

    fixed sqlite connection

--- main-server/main-server.py
+++ main-server/main-server.py
@@ -2,22 +2,20 @@ from flask import Flask, request, render_template
 from sqlalchemy import create_engine
 import sqlite3
 app = Flask(__name__)
-#con = sqlite3.connect('earthquake_data.db')
 @app.route('/post', methods=["POST"])
 def postJsonHandler():
     data = request.get_json()
     ax= data['values'][0]
     ay= data['values'][1]
     az= data['values'][2]
-    try:
-        with sql.connect("earthquake_data.db") as con:
+    print(ax,ay,az)
+    with sqlite3.connect("earthquake_data.db") as con:
+            print(ax,ay,az)
             cur = con.cursor()
             cur.execute("INSERT INTO Acceleration (X_dir,Y_dir,Z_dir)\
                VALUES (?,?,?)",(ax,ay,az) )
             con.commit()
             msg = "Record successfully added"
-    except:
-        pass
     return ''
 @app.route('/', methods=["GET"])
 def get():
