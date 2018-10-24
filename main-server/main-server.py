609816682ce82926cbdd6e97d494d6a7a8d8002e
Ubuntu <darkside@main-sv.b0ovjr353ggu1h2h0pdfs3jd2d.bx.internal.cloudapp.net>
2018-10-24 14:09:06 +0000

    fixed static url resolver

--- main-server/main-server.py
+++ main-server/main-server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from sqlalchemy import create_engine
 import sqlite3
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='', static_folder='static')
 @app.route('/post', methods=["POST"])
 def postJsonHandler():
     data = request.get_json()
