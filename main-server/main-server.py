27b73a19fe6917cd35814c9b0eade4ac92960ce9
Anshul Ahuja <36476228+anshulahuja98@users.noreply.github.com>
2018-10-26 01:27:37 +0530

    added Chart.js based graphs

--- main-server/main-server.py
+++ main-server/main-server.py
@@ -2,6 +2,12 @@ from flask import Flask, request, render_template
 import sqlite3
 
 app = Flask(__name__, static_url_path='', static_folder='static')
+with sqlite3.connect("earthquake_data.db") as con:
+    cur = con.cursor()
+    cur.execute("INSERT INTO Acceleration (X_dir,Y_dir,Z_dir)\
+              VALUES (?,?,?)", (1, 1, 1))
+    con.commit()
+    msg = "Record successfully added"
 
 
 @app.route('/post', methods=["POST"])
@@ -24,6 +30,21 @@ def postJsonHandler():
 @app.route('/', methods=["GET"])
 def get():
     return render_template('index.html')
+
+
+@app.route("/chart", methods=["GET"])
+def chart():
+    with sqlite3.connect("earthquake_data.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Acceleration ")
+        rows = cur.fetchall()
+        print(rows)
+        labels = []
+        values = []
+        for row in rows:
+            values = values + [row[0], ]
+            labels = labels + [row[3], ]
+    return render_template('chart.html', values=values, labels=labels)
 
 
 app.run(host='0.0.0.0', port=80, debug=True)
--- raspberry-pi/flask-sv.py
+++ raspberry-pi/flask-sv.py
@@ -3,35 +3,40 @@ import time
 from flask import Flask, request
 import vlc
 import requests
+
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW)
+
 URL = "http://disaster-chain.eastus.cloudapp.azure.com/post"
+
+
 @app.route('/post', methods=["POST"])
 def postJsonHandler():
     data = request.get_json()
     print(data['values'])
-    mod_ax= abs(data['values'][0])
-    mod_ay= abs(data['values'][1])
-    mod_az= abs(data['values'][2])
-   # data1= {'Ax':data['values'][0],
-    #        'Ay':data['values'][1],
-     #       'Az':data['values'][2]}
-    r= requests.post(url= URL,json= data)
+    mod_ax = abs(data['values'][0])
+    mod_ay = abs(data['values'][1])
+    mod_az = abs(data['values'][2])
+    r = requests.post(url=URL, json=data)
     print(r)
-    if mod_ax>1.5 or mod_ay>1.5 or mod_az>1.5:
+    if mod_ax > 1.5 or mod_ay > 1.5 or mod_az > 1.5:
         print("Earthquake Detected")
-        GPIO.output(18,GPIO.HIGH)
-        GPIO.output(19,GPIO.HIGH)
+        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(19, GPIO.HIGH)
         player = vlc.MediaPlayer("/home/pi/CFD-18/raspberry-pi/siren.mp3")
         player.play()
         time.sleep(24)
     else:
         print("You are safe")
     return ''
+
+
 @app.route('/get', methods=["GET"])
 def get():
     return 'GET'
+
 
 app.run(host='0.0.0.0', port=8090, debug=True)
