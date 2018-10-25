4cf9183a187ac1708276e10262b63c51a43d73f9
Anshul Ahuja <36476228+anshulahuja98@users.noreply.github.com>
2018-10-25 14:57:18 +0530

    code cleanup

--- main-server/main-server.py
+++ main-server/main-server.py
@@ -1,24 +1,29 @@
 from flask import Flask, request, render_template
-from sqlalchemy import create_engine
 import sqlite3
-app = Flask(__name__,static_url_path='', static_folder='static')
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+
 @app.route('/post', methods=["POST"])
 def postJsonHandler():
     data = request.get_json()
-    ax= data['values'][0]
-    ay= data['values'][1]
-    az= data['values'][2]
-    print(ax,ay,az)
+    ax = data['values'][0]
+    ay = data['values'][1]
+    az = data['values'][2]
+    print(ax, ay, az)
     with sqlite3.connect("earthquake_data.db") as con:
-            print(ax,ay,az)
-            cur = con.cursor()
-            cur.execute("INSERT INTO Acceleration (X_dir,Y_dir,Z_dir)\
-               VALUES (?,?,?)",(ax,ay,az) )
-            con.commit()
-            msg = "Record successfully added"
+        print(ax, ay, az)
+        cur = con.cursor()
+        cur.execute("INSERT INTO Acceleration (X_dir,Y_dir,Z_dir)\
+               VALUES (?,?,?)", (ax, ay, az))
+        con.commit()
+        msg = "Record successfully added"
     return ''
+
+
 @app.route('/', methods=["GET"])
 def get():
-    return render_template('index.html') 
+    return render_template('index.html')
+
 
 app.run(host='0.0.0.0', port=80, debug=True)
