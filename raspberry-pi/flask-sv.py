52202bfeb0a331d626e831459f2413194c0f5a60
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-27 09:50:12 +0000

    Updated pi code with location get req

--- raspberry-pi/flask-sv.py
+++ raspberry-pi/flask-sv.py
@@ -1,4 +1,3 @@
-import RPi.GPIO as GPIO
 import time
 from flask import Flask, request
 import vlc
@@ -7,14 +6,9 @@ import array as arr
 import numpy as np
 
 app = Flask(__name__)
-GPIO.cleanup()
-GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(33, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(37, GPIO.OUT, initial=GPIO.HIGH)
-
-URL = "http://disaster-chain.eastus.cloudapp.azure.com/post"
+#URL = "http://disaster-chain.eastus.cloudapp.azure.com/post"
+URL = "192.168.31.123:8000/post"
 player = vlc.MediaPlayer("/home/pi/CFD-18/raspberry-pi/siren.mp3")
 x=0
 y=0
@@ -28,7 +22,6 @@ def postJsonHandler():
     global x,y
     global dev1, dev2
     data = request.get_json()
-    print(data['values'])
     if data['deviceid'] == 1111: 
         dev1[0][x]= abs(data['values'][0])
         dev1[1][x]= abs(data['values'][1])
@@ -41,7 +34,8 @@ def postJsonHandler():
         dev2[2][y]= abs(data['values'][2])
         dev2[3][y]= abs(data['values'][3])
         y= y+1
-	print(dev2)
+    print(dev1)
+    print(dev2)
     if x > p-1: 
         x = 0
     if y > q-1:
@@ -52,14 +46,16 @@ def postJsonHandler():
     avg_dev2 = (horizontal_avg_dev2[0]+horizontal_avg_dev2[1]+horizontal_avg_dev2[2])/3
     if (horizontal_avg_dev1[0]>1 or horizontal_avg_dev1[1] >1 or horizontal_avg_dev1[2]>1) and (horizontal_avg_dev2[0]>1 or horizontal_avg_dev2[1]>1 or horizontal_avg_dev2[0] >1):
         global flag_device
-	flag_device = True
-        GPIO.output(37, 0)
+        flag_device = True
+	coordinates = requests.get("http://api.ipstack.com/check?access_key=e8b0704f2fdaaa7d0f648d8e070916a3&fields=longitude,latitude").json()
+	data['latitude'] =  coordinates['latitude']
+	data['longitude'] =  coordinates['longitude']
         if not player.is_playing():
             player.play()
             time.sleep(5)
-	GPIO.output(33,1)
-	GPIO.output(31,1)
-    	r = requests.post(url=URL, json=data)
+	r = requests.post(url=URL, json=data)
+	print("Earthquake!!")
+	
     else:
         print("You are safe")
     return ''
@@ -71,7 +67,7 @@ def get():
 	return "12"
 	flag_device == False
    else:
-	return ""
+	return "123"
 
 
 app.run(host='0.0.0.0', port=8090, debug=True)
