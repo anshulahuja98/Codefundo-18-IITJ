ccfeb5ed18e48674b057ff7aa5000fda75b7c958
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-27 14:50:54 +0530

    Plot maps on main server

--- main-server/main-server.py
+++ main-server/main-server.py
@@ -2,17 +2,22 @@ from flask import Flask, request, render_template
 import sqlite3
 
 app = Flask(__name__, static_url_path='', static_folder='static')
-with sqlite3.connect("earthquake_data.db") as con:
-    cur = con.cursor()
-    cur.execute("INSERT INTO Acceleration (X_dir,Y_dir,Z_dir)\
-              VALUES (?,?,?)", (1, 1, 1))
-    con.commit()
-    msg = "Record successfully added"
+# with sqlite3.connect("earthquake_data.db") as con:
+
+
+# cur = con.cursor()
+# cur.execute("INSERT INTO Acceleration (X_dir,Y_dir,Z_dir)\
+#           VALUES (?,?,?)", (1, 1, 1))
+# con.commit()
+# msg = "Record successfully added"
 
 
 @app.route('/post', methods=["POST"])
 def postJsonHandler():
     data = request.get_json()
+    print(data)
+    latitude = data['latitude']
+    longitude = data['longitude']
     ax = data['values'][0]
     ay = data['values'][1]
     az = data['values'][2]
@@ -22,6 +27,9 @@ def postJsonHandler():
         cur = con.cursor()
         cur.execute("INSERT INTO Acceleration (X_dir,Y_dir,Z_dir)\
                VALUES (?,?,?)", (ax, ay, az))
+        cur.execute("INSERT INTO Coordinates (latitude,longitude)\
+                   VALUES (?,?,?)", (latitude, longitude))
+
         con.commit()
         msg = "Record successfully added"
     return ''
@@ -47,4 +55,27 @@ def chart():
     return render_template('chart.html', values=values, labels=labels)
 
 
-app.run(host='0.0.0.0', port=80, debug=True)
+@app.route("/maps", methods=["GET"])
+def maps():
+    with sqlite3.connect("earthquake_data.db") as con:
+        cur = con.cursor()
+        # cur.execute("SELECT * FROM Acceleration ")
+        # rows = cur.fetchall()
+        rows = []
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Coordinates ")
+        coord_rows = cur.fetchall()
+        print(rows)
+        print(coord_rows)
+        labels = []
+        values = []
+        for row in rows:
+            values = values + [row[0], ]
+            labels = labels + [row[3], ]
+        # for row in coord_rows:
+        # values = latitude + [coord_rows[0], ]
+        # labels = labels + [coord_rows[1], ]
+    return render_template('maps.html', values=values, labels=labels, coordinates=coord_rows)
+
+
+app.run(host='0.0.0.0', port=8000, debug=True)
