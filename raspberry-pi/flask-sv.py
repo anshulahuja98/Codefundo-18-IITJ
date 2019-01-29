9aa8304fa9c8ac2c523e8ec128dd7f6b60b83a1a
Anshul Ahuja <anshul.ahu@gmail.com>
2019-01-29 19:18:07 +0000

    Update raspi edited code

--- raspberry-pi/flask-sv.py
+++ raspberry-pi/flask-sv.py
@@ -4,15 +4,18 @@ import vlc
 import requests
 import array as arr
 import numpy as np
-
+import RPi.GPIO as GPIO
 app = Flask(__name__)
-
-#URL = "http://disaster-chain.eastus.cloudapp.azure.com/post"
-URL = "192.168.31.123:8000/post"
+GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
+GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(33, GPIO.OUT, initial=GPIO.LOW)
+#URL = "http://disaster-chain-1.eastus.cloudapp.azure.com/post"
+URL = "http://192.168.31.7/post"
 player = vlc.MediaPlayer("/home/pi/CFD-18/raspberry-pi/siren.mp3")
 x=0
 y=0
-p,q=5,5
+p,q=3,3
 dev1 = np.zeros([4,p])
 dev2 = np.zeros([4,p])
 flag_device = False
@@ -42,20 +45,22 @@ def postJsonHandler():
         y = 0
     horizontal_avg_dev1=np.mean(dev1, axis=1)
     horizontal_avg_dev2=np.mean(dev2, axis=1)
-    avg_dev1 = (horizontal_avg_dev1[0]+horizontal_avg_dev1[1]+horizontal_avg_dev1[2])/3
-    avg_dev2 = (horizontal_avg_dev2[0]+horizontal_avg_dev2[1]+horizontal_avg_dev2[2])/3
-    if (horizontal_avg_dev1[0]>1 or horizontal_avg_dev1[1] >1 or horizontal_avg_dev1[2]>1) and (horizontal_avg_dev2[0]>1 or horizontal_avg_dev2[1]>1 or horizontal_avg_dev2[0] >1):
+    if (horizontal_avg_dev1[0]>1.5 or horizontal_avg_dev1[1] >1.5 or horizontal_avg_dev1[2]>1.5) or (horizontal_avg_dev2[0]>1 or horizontal_avg_dev2[1]>1 or horizontal_avg_dev2[0] >1):
         global flag_device
         flag_device = True
 	coordinates = requests.get("http://api.ipstack.com/check?access_key=e8b0704f2fdaaa7d0f648d8e070916a3&fields=longitude,latitude").json()
-	data['latitude'] =  coordinates['latitude']
-	data['longitude'] =  coordinates['longitude']
+	#data['latitude'] =  coordinates['latitude']
+	#data['longitude'] =  coordinates['longitude']
+        r = requests.post(url=URL, json=data)
+	GPIO.output(31, 1)
+        GPIO.output(33, 1)
         if not player.is_playing():
             player.play()
             time.sleep(5)
-	r = requests.post(url=URL, json=data)
 	print("Earthquake!!")
-	
+    if (horizontal_avg_dev1[3]>60 or horizontal_avg_dev2[3]>60):
+	    flag_device = True
+	    print("Fire detected!!!!")
     else:
         print("You are safe")
     return ''
@@ -64,10 +69,9 @@ def postJsonHandler():
 @app.route('/get', methods=["GET"])
 def get():
    if  flag_device ==True:
-	return "12"
-	flag_device == False
+	return '1'
    else:
-	return "123"
+	return '0'
 
 
 app.run(host='0.0.0.0', port=8090, debug=True)
